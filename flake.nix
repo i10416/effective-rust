b36e50bba27a16f4334c5d766b31ecf83bfbb06b
i10416 <ito.yo16uh90616@gmail.com>
2025-01-06 15:27:34 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Effective Rust";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      rust-overlay,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain).override {
+            extensions = [ "rust-src" ];
+          };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+          devShells = {
+            default = pkgs.mkShell {
+              name = "Effective Rust Workspace";
+              buildInputs = with pkgs; [
+                git
+                curl
+                unzip
+                toolchain
+              ];
+            };
+          };
+        };
+    };
+}
